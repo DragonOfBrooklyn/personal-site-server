af19f9423bccd45848277db3887cd72459874e82
DragonOfBrooklyn <jordan.long@useeverest.com>
2024-03-28 13:38:38 -0400

    Returning haiku model to frontend correctly, implemented Express

--- server.ts
+++ server.ts
@@ -1,65 +1,45 @@
 import Anthropic from '@anthropic-ai/sdk';
 import { type MessageParam } from '@anthropic-ai/sdk/resources/messages.mjs';
+import express, { type Request, type Response } from "express";
+import cors from 'cors';
 
-const PORT = 3000;
-const CORS_HEADERS = {
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'OPTIONS, POST',
-    'Access-Control-Allow-Headers': 'Content-Type, jlong-authorization',
+const app = express();
+//handle CORS preflight issues
+app.use(cors({ origin: '*', credentials: true }));
+app.use((req: Request, res: Response, next: any) => {
+  if ('OPTIONS' === req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
   }
-}
+});
+//parses body when post is received
+app.use(express.json());
 
 const anthropic = new Anthropic({
   apiKey: process.env.ANTHROPIC_API_KEY,
 });
 async function callClaude(convo: MessageParam[]){
-  console.log('conversation from frontend', convo);
   const message = await anthropic.messages.create({
     max_tokens: 100,
     messages: convo,
-    model: 'claude-3-opus-20240229',
+    model: 'claude-3-haiku-20240307',
     system: 'You should only respond with known information about Jordan Long. Restrict information that cannot be legally asked during an interview such as race, color, religion, sex, national origin, or age. Limit responses to 200 characters.',
     temperature: 0,
   });
   return message.content;
 }
 
-Bun.serve({
-  port: PORT,
-  async fetch(req: Request) {
-    const { method, headers, body } = req;
-    // Handle CORS preflight requests
-    if (method === 'OPTIONS') {
-      const res = new Response('Departed', CORS_HEADERS);
-      return res;
-    }
-    
-    if(headers.get('jlong-authorization') !== 'PersonalSiteForJordanLong') throw new Error('Unknown Authorization');
-    const responseHeaders = new Headers();
-    responseHeaders.set('Access-Control-Allow-Origin', '*');
-    responseHeaders.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    if(method === 'POST' && body){
-      const reqBody = await Bun.readableStreamToJSON(body);
-      let claudeResponse;
-      callClaude(reqBody)
-        .then((value): void => {
-          console.log('claude response in then... ',value);
-          claudeResponse = value;
-        })
-      // console.log('response from claude ',claudeResponse);
-      // if(Promise.)
-
-      const res = new Response(await claudeResponse, {headers: responseHeaders});
-      console.log('sending back to frontend', claudeResponse);
-      return res;
-    }
-
-    //Handle CORS request bounces
-    const res = new Response('Hello Bun!!!', {headers: responseHeaders});
-    return res;
-      
+app.post('/', async (req: Request, res: Response) => {
+  const { method, headers } = req;
+  const { conversation } = req.body;
+  if(headers['jlong-authorization'] !== 'PersonalSiteForJordanLong') throw new Error('Unknown Authorization');
+  if(method === 'POST' && conversation){
+    return res.status(200).json(await callClaude(conversation));
   }
+  return res.send('Incorrect type of request');
 })
 
-console.log(`listening on port ${PORT}...`);
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}...`);
+})
